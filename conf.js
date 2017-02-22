a089ba7269353781e6670d0e71f9163bda77f785
CORP\risjogre <rickard.sjogren@sogeti.se>
2017-02-22 11:46:44 +0100

    conf and spec for using html reporter. Will add the repo next

---
+++ conf.js
@@ -0,0 +1,23 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
+
+exports.config = {
+  framework: 'jasmine2',
+  capabilities: {
+		'browserName': 'chrome'
+	},
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['todo-spec.js'],
+  
+  onPrepare: function() {
+   	
+      jasmine.getEnv().addReporter(
+        new Jasmine2HtmlReporter({
+          savePath: 'target/screenshots'
+        })
+      );
+   }
+};
