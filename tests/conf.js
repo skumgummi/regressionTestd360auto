c4b23559fabfd7382e69148d48a3d6ec8b5caacc
CORP\risjogre <rickard.sjogren@sogeti.se>
2017-02-23 13:18:21 +0100

    Updated conf.js with reporter function

--- tests/conf.js
+++ tests/conf.js
@@ -1,7 +1,22 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['bookStreamProof.js'],
+  framework: 'jasmine2',
   capabilities: {
 		'browserName': 'chrome'
-	}
+	},
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['bookStreamProof.js'],
+  
+  onPrepare: function() {
+   	
+      jasmine.getEnv().addReporter(
+        new Jasmine2HtmlReporter({
+          savePath: 'target/screenshots'
+        })
+      );
+   }
 };
