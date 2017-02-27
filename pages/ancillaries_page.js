f7241180cf5b82846c7d95b5e6bae120956038cb
Niklas Ekstrand <niklas.ekstrand@sogeti.se>
2017-02-27 14:29:15 +0100

    fixed some more typos

--- pages/ancillaries_page.js
+++ pages/ancillaries_page.js
@@ -2,8 +2,8 @@ var ancillariesPage = function(){
   this.shoppingCartButton = element(by.buttonText('CONTINUE'));
 
   this.selectSeatButton = element(by.css('img[src="images/seat-img.jpg"]'));
-  this.selectSeatOutbound = element(by.xpath("//*[@id="segment-container"]/div[2]/ul/li[1]/a"));
-  this.selectSeatReturn = element(by.xpath("//*[@id="segment-container"]/div[2]/ul/li[2]/a"));
+  this.selectSeatOutbound = element(by.xpath('//*[@id="segment-container"]/div[2]/ul/li[1]/a'));
+  this.selectSeatReturn = element(by.xpath('//*[@id="segment-container"]/div[2]/ul/li[2]/a'));
 
   this.selectBagsButton = element(by.css('img[src="images/bag-img.jpg"]'));
   this.addBaggageOutbound = element(by.id('add_0_0'));
@@ -15,13 +15,13 @@ var ancillariesPage = function(){
   this.bagCancelButton = element(by.binding('translation["ancillaryxbags.cancel"]'));
 
   this.addMealButton = element(by.css('img[alt="Select Meal"]'));
-  this.selectMealOutbound = element(by.xpath("//*[@id="segment-container"]/div[2]/ul/li[1]/a"));
-  this.selectMealReturn = element(by.xpath("//*[@id="segment-container"]/div[2]/ul/li[2]/a"));
+  this.selectMealOutbound = element(by.xpath('//*[@id="segment-container"]/div[2]/ul/li[1]/a'));
+  this.selectMealReturn = element(by.xpath('//*[@id="segment-container"]/div[2]/ul/li[2]/a'));
   this.mealDropDown = element(by.css('.btn.btn-default.paidMealsBtn'));
-  this.mealDropDown1 = element(by.xpath("//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[2]"));
-  this.mealDropDown2 = element(by.xpath("//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[3]"));
-  this.mealDropDown2 = element(by.xpath("//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[4]"));
-  this.mealDropDown2 = element(by.xpath("//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[5]"));
+  this.mealDropDown1 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[2]'));
+  this.mealDropDown2 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[3]'));
+  this.mealDropDown2 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[4]'));
+  this.mealDropDown2 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[5]'));
 
   //Got stuck on the buttons used to switch between outbound and return flight, no unique identifiers? trying xpath
 
