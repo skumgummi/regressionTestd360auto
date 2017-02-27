3afa1c7496afb5965a11a4cf7d273313651f9349
Niklas Ekstrand <niklas.ekstrand@sogeti.se>
2017-02-27 10:23:02 +0100

    added buttons to ancillaries_page.js

--- pages/ancillaries_page.js
+++ pages/ancillaries_page.js
@@ -1,5 +1,19 @@
 var ancillariesPage = function(){
   this.shoppingCartButton = element(by.buttonText('CONTINUE'));
+  this.selectSeatButton = element(by.css('img[src="images/seat-img.jpg"]'));
+
+  this.selectBagsButton = element(by.css('img[src="images/bag-img.jpg"]'));
+  this.addBaggageOutbound = element(by.id('add_0_0'));
+  this.removeBaggageOutbound = element(by.id('remove_0_0'));
+  this.addBaggageReturn = element(by.id('add_0_1'));
+  this.removeBaggageReturn = element(by.id('remove_0_1'));
+  this.bagAddToBooking = element(by.binding('translation["ancillaryxbags.addToBooking"]'));
+  this.freeBaggageInfo = element(by.binding('translation["ancillaryxbags.freeBaggageInfo"]'));
+  this.bagCancelButton = element(by.binding('translation["ancillaryxbags.cancel"]'));
+
+  this.addMealButton = element(by.css('img[alt="Select Meal"]'));
+
+  //Got stuck on the buttons used to switch between outbound and return flight, no unique identifiers?
 
 };
 module.exports = new ancillariesPage();
