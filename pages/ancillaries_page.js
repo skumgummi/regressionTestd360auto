3b9e1ab50ef3894e0f3156dee2109237626266c5
Niklas Ekstrand <niklas.ekstrand@sogeti.se>
2017-02-27 15:07:57 +0100

    minor fixes

--- pages/ancillaries_page.js
+++ pages/ancillaries_page.js
@@ -18,10 +18,10 @@ var ancillariesPage = function(){
   this.selectMealOutbound = element(by.xpath('//*[@id="segment-container"]/div[2]/ul/li[1]/a'));
   this.selectMealReturn = element(by.xpath('//*[@id="segment-container"]/div[2]/ul/li[2]/a'));
   this.mealDropDown = element(by.css('.btn.btn-default.paidMealsBtn'));
-  this.mealDropDown1 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[2]'));
-  this.mealDropDown2 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[3]'));
-  this.mealDropDown3 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[4]'));
-  this.mealDropDown4 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[5]'));
+  this.mealDropDownOption1 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[2]'));
+  this.mealDropDownOption2 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[3]'));
+  this.mealDropDownOption3 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[4]'));
+  this.mealDropDownOption4 = element(by.xpath('//*[@id="segment-container"]/div[4]/div[1]/div[1]/div/div[2]/div/ul/li[5]'));
 
   //Got stuck on the buttons used to switch between outbound and return flight, no unique identifiers? trying xpath
 
